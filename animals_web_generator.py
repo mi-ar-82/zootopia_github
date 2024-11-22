8fa08aa0deda17d9d01ee1f0613590791f1c0752
m <m@m.com>
2024-11-22 19:22:15 +0100

    bring animal data into html: read data from json create string replace str with string write new html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,17 +5,37 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-animals_data = load_data('animals_data.json')
+def generate_animal_info(data):
+    output = ''
+    for animal in data:
+        if 'name' in animal:
+            output += f"Name: {animal['name']}<br>\n"
+        if 'characteristics' in animal and 'diet' in animal['characteristics']:
+            output += f"Diet: {animal['characteristics']['diet']}<br>\n"
+        if 'locations' in animal and animal['locations']:
+            output += f"Location: {animal['locations'][0]}<br>\n"
+        if 'characteristics' in animal and 'type' in animal['characteristics']:
+            output += f"Type: {animal['characteristics']['type']}<br>\n"
+        output += "<br>\n"  # Add a blank line between animals
+    return output
 
 
-for animal in animals_data:
-    if 'name' in animal:
-        print(f"Name: {animal['name']}")
-    if 'characteristics' in animal and 'diet' in animal['characteristics']:
-        print(f"Diet: {animal['characteristics']['diet']}")
-    if 'locations' in animal and animal['locations']:
-        print(f"Location: {animal['locations'][0]}")
-    if 'characteristics' in animal and 'type' in animal['characteristics']:
-        print(f"Type: {animal['characteristics']['type']}")
+def update_html_template(template_path, output_path, animals_info):
+    with open(template_path, "r") as file:
+        html_content = file.read()
     
-    print()
+    updated_content = html_content.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+    
+    with open(output_path, "w") as file:
+        file.write(updated_content)
+
+
+json_file_path = "animals_data.json"
+template_file_path = "animals_template.html"
+output_file_path = "animals.html"
+
+animals_data = load_data(json_file_path)
+
+animals_info_string = generate_animal_info(animals_data)
+
+update_html_template(template_file_path, output_file_path, animals_info_string)
